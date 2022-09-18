e45f2fb6cf13a33d5d299dbd88aabf1529177e6c
Fatma Ozturk <93829110+Fatma-Ozturk@users.noreply.github.com>
2022-09-18 18:12:01 +0300

    My first swift project
    simple calculator with basic math

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstnumber: UITextField!
+    
+    @IBOutlet weak var secondnumber: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sum(_ sender: Any) {
+        if let first = Int(firstnumber.text!) {
+            if let second = Int(secondnumber.text!) {
+                result.text = String(first + second)
+            }
+        }
+    }
+    
+    @IBAction func substraction(_ sender: Any) {
+        if let first = Int(firstnumber.text!) {
+            if let second = Int(secondnumber.text!) {
+                result.text = String(first - second)
+            }
+        }
+    }
+    
+    @IBAction func multiple(_ sender: Any) {
+        if let first = Int(firstnumber.text!) {
+            if let second = Int(secondnumber.text!) {
+                result.text = String(first * second)
+            }
+        }
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+        if let first = Int(firstnumber.text!) {
+            if let second = Int(secondnumber.text!) {
+                result.text = String(first / second)
+            }
+        }
+    }
 }
 
